b7c7004484d1d86586abb597740dcf6dd6550f7f
anhpham77346 <anhpham77346@gmail.com>
2024-11-16 16:02:18 +0700

    opttimize

--- js/app.js
+++ js/app.js
@@ -10,13 +10,9 @@ var AudioContext = window.AudioContext || window.webkitAudioContext;
 var audioContext //audio context to help us record
 
 var recordButton = document.getElementById("recordButton");
-// var stopButton = document.getElementById("stopButton");
-// var pauseButton = document.getElementById("pauseButton");
 
 //add events to those 2 buttons
 recordButton.addEventListener("click", startRecording);
-// stopButton.addEventListener("click", stopRecording);
-// pauseButton.addEventListener("click", pauseRecording);
 
 // Speech Recognition Variables
 const webkitSpeechRecognition = window.webkitSpeechRecognition;
@@ -65,8 +61,6 @@ function startRecording() {
 	*/
 
 	recordButton.disabled = true;
-	// stopButton.disabled = false;
-	// pauseButton.disabled = false
 
 	recognition.start();
 
@@ -113,37 +107,14 @@ function startRecording() {
 		}, 5000);
 
 	}).catch(function(err) {
-	  	//enable the record button if getUserMedia() fails
     	recordButton.disabled = false;
-    	// stopButton.disabled = true;
-    	// pauseButton.disabled = true
 	});
 }
-
-// function pauseRecording(){
-// 	console.log("pauseButton clicked rec.recording=",rec.recording );
-// 	if (rec.recording){
-// 		//pause
-// 		rec.stop();
-// 		pauseButton.innerHTML="Resume";
-// 	}else{
-// 		//resume
-// 		rec.record()
-// 		pauseButton.innerHTML="Pause";
-
-// 	}
-// }
 
 function stopRecording() {
 	console.log("stopButton clicked");
 
-	//disable the stop button, enable the record too allow for new recordings
-	// stopButton.disabled = true;
 	recordButton.disabled = false;
-	// pauseButton.disabled = true;
-
-	//reset button just in case the recording is stopped while paused
-	// pauseButton.innerHTML="Pause";
 	
 	//tell the recorder to stop the recording
 	rec.stop();
@@ -151,59 +122,7 @@ function stopRecording() {
 	//stop microphone access
 	gumStream.getAudioTracks()[0].stop();
 
-	//create the wav blob and pass it on to createDownloadLink
-	// rec.exportWAV(createDownloadLink);
 	rec.exportWAV(sendWAVtoCloud);
-}
-
-function createDownloadLink(blob) {
-	var url = URL.createObjectURL(blob);
-	var au = document.createElement('audio');
-	var li = document.createElement('li');
-	var link = document.createElement('a');
-
-	//name of .wav file to use during upload and download (without extendion)
-	var filename = new Date().toISOString();
-
-	//add controls to the <audio> element
-	au.controls = true;
-	au.src = url;
-
-	// save to disk link
-	link.href = url;
-	link.download = filename+".wav"; //download forces the browser to donwload the file using the  filename
-	link.innerHTML = "Save to disk";
-
-	// add the new audio element to li
-	li.appendChild(au);
-	
-	//add the filename to the li
-	li.appendChild(document.createTextNode(filename+".wav "))
-
-	//add the save to disk link to li
-	li.appendChild(link);
-	
-	// upload link
-	var upload = document.createElement('a');
-	upload.href="#";
-	upload.innerHTML = "Upload";
-	upload.addEventListener("click", function(event){
-		  var xhr=new XMLHttpRequest();
-		  xhr.onload=function(e) {
-		      if(this.readyState === 4) {
-		          console.log("Server returned: ",e.target.responseText);
-		      }
-		  };
-		  var fd=new FormData();
-		  fd.append("audio_data",blob, filename);
-		  xhr.open("POST","upload.php",true);
-		  xhr.send(fd);
-	})
-	li.appendChild(document.createTextNode (" "))//add a space in between
-	li.appendChild(upload)//add the upload link to li
-
-	// add the li element to the ol
-	recordingsList.appendChild(li);
 }
 
 function sendWAVtoCloud(blob) {
