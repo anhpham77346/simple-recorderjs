2b3240782281e65c4dd3babe8b42a49082fb5f17
anhpham77346 <anhpham77346@gmail.com>
2024-11-16 14:38:06 +0700

    optimize

--- js/app.js
+++ js/app.js
@@ -10,13 +10,13 @@ var AudioContext = window.AudioContext || window.webkitAudioContext;
 var audioContext //audio context to help us record
 
 var recordButton = document.getElementById("recordButton");
-var stopButton = document.getElementById("stopButton");
-var pauseButton = document.getElementById("pauseButton");
+// var stopButton = document.getElementById("stopButton");
+// var pauseButton = document.getElementById("pauseButton");
 
 //add events to those 2 buttons
 recordButton.addEventListener("click", startRecording);
-stopButton.addEventListener("click", stopRecording);
-pauseButton.addEventListener("click", pauseRecording);
+// stopButton.addEventListener("click", stopRecording);
+// pauseButton.addEventListener("click", pauseRecording);
 
 function startRecording() {
 	console.log("recordButton clicked");
@@ -33,8 +33,8 @@ function startRecording() {
 	*/
 
 	recordButton.disabled = true;
-	stopButton.disabled = false;
-	pauseButton.disabled = false
+	// stopButton.disabled = false;
+	// pauseButton.disabled = false
 
 	/*
     	We're using the standard promise based getUserMedia() 
@@ -72,38 +72,41 @@ function startRecording() {
 
 		console.log("Recording started");
 
+		// Automatically stop recording after 5 seconds
+		setTimeout(stopRecording, 5000); // 5000 milliseconds = 5 seconds
+
 	}).catch(function(err) {
 	  	//enable the record button if getUserMedia() fails
     	recordButton.disabled = false;
-    	stopButton.disabled = true;
-    	pauseButton.disabled = true
+    	// stopButton.disabled = true;
+    	// pauseButton.disabled = true
 	});
 }
 
-function pauseRecording(){
-	console.log("pauseButton clicked rec.recording=",rec.recording );
-	if (rec.recording){
-		//pause
-		rec.stop();
-		pauseButton.innerHTML="Resume";
-	}else{
-		//resume
-		rec.record()
-		pauseButton.innerHTML="Pause";
+// function pauseRecording(){
+// 	console.log("pauseButton clicked rec.recording=",rec.recording );
+// 	if (rec.recording){
+// 		//pause
+// 		rec.stop();
+// 		pauseButton.innerHTML="Resume";
+// 	}else{
+// 		//resume
+// 		rec.record()
+// 		pauseButton.innerHTML="Pause";
 
-	}
-}
+// 	}
+// }
 
 function stopRecording() {
 	console.log("stopButton clicked");
 
 	//disable the stop button, enable the record too allow for new recordings
-	stopButton.disabled = true;
+	// stopButton.disabled = true;
 	recordButton.disabled = false;
-	pauseButton.disabled = true;
+	// pauseButton.disabled = true;
 
 	//reset button just in case the recording is stopped while paused
-	pauseButton.innerHTML="Pause";
+	// pauseButton.innerHTML="Pause";
 	
 	//tell the recorder to stop the recording
 	rec.stop();
